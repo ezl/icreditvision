1c994ba7ba005a8812a46edf51edabffe2a34743
ezl <ericzliu@gmail.com>
2010-12-10 12:47:27 -0600

    init -- functional icreditvision api hits

---
+++ icreditvision.py
@@ -0,0 +1,64 @@
+import urllib2
+from urllib import urlencode
+from xmltodict import xmltodict
+
+class CreditVision(object):
+    base_url = "https://www.icreditvisions.com/cgi-bin/query.pl"
+    def __init__(self, login="leaselycom1", password="zliu1142"):
+        self.credentials = dict(login=login, password=password)
+
+    @staticmethod
+    def retrieve_url(url, data=None):
+        request = urllib2.Request(url, data)
+        response = urllib2.urlopen(request)
+        return response.read()
+
+    def add(self):
+        """http://www.icreditvision.com/cvwrapi/add.html"""
+        required_fields = dict(source="api",
+                               top="Process",
+                               bottom="Process",
+                               a_tu="Y",
+                              )
+        sample_customer = dict(a_lname="AKACOMMON",
+                               a_fname="SILVIA",
+                               a_ssno="435-70-9958",
+                               ca_house="9",
+                               ca_street_name="98TH+DR",
+                               ca_city="FANTASY+ISLAND",
+                               ca_state="IL",
+                               ca_zip="60750",
+                              )
+        data = dict(mode="add")
+        data.update(self.credentials)
+        data.update(required_fields)
+        data.update(sample_customer)
+        """returns 4853091 3706304"""
+        return self.retrieve_url(self.base_url, urlencode(data))
+
+    def view(self, controlno="4853296", dcontrolno_key="370651"):
+        """This is REALLY slow. as long as 60 seconds."""
+        format = dict(formatHTML="N",
+                      formatXML="Y",
+                      formatMISMO="N",
+                      formatASCII="N",
+                      formatPDF="N",
+                     )
+        data = dict(mode="view")
+        data.update(self.credentials)
+        data.update(format)
+        data['controlno'] = controlno
+        data['dcontrolno_key'] = dcontrolno_key
+        return self.retrieve_url(self.base_url, urlencode(data))
+
+    def status(self):
+        pass
+
+    def list(self):
+        pass
+
+api = CreditVision()
+codes_raw = api.add()
+
+report_raw = api.view()
+report_dict = xmltodict(report)
