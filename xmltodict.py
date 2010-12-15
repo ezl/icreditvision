c92cafb45de66942d06a3d3018bb7203d90689ae
ezl <ericzliu@gmail.com>
2010-12-15 13:56:08 -0600

    hehehaha

--- xmltodict.py
+++ xmltodict.py
@@ -19,7 +19,9 @@ def elementtodict(parent):
                 d[child.tagName]
             except KeyError:
                 d[child.tagName]=[]
-            d[child.tagName].append(elementtodict(child))
+# -            d[child.tagName].append(elementtodict(child))
+# +            d[child.tagName] = elementtodict(child)
+            d[child.tagName] = elementtodict(child)
         child = child.nextSibling
     return d
 
@@ -35,8 +37,3 @@ def remove_whilespace_nodes(node, unlink=True):
         if unlink:
             node.unlink()
 
-test = """
-<RESPONSE_GROUP MISMOVersionID="2.2"><RESPONDING_PARTY Name="Zip Reports" _StreetAddress="7529 Sunset Ave. Suite C-4" _City="FAIR OAKS" _State="CA" _PostalCode="95628"><CONTACT_DETAIL><CONTACT_POINT _Type="Phone" _Value="8003111585" /></CONTACT_DETAIL></RESPONDING_PARTY><RESPOND_TO_PARTY _Name="LEASELY " _StreetAddress="1142 FLORIDA ST" _StreetAddress2="" _City="SAN FRANCISCO" _State="CA" _PostalCode="94110"><CONTACT_DETAIL _Name="ERIC LIU  " /></RESPOND_TO_PARTY><RESPONSE ResponseDateTime="2010-12-10T10:21:40"><KEY _Name="Calyx Transaction ID" _Value="default" /><STATUS _Condition="Error" _Code="-1" _Description="Error - the Reference Number is not valid" /><RESPONSE_DATA><CREDIT_RESPONSE MISMOVersionID="2.2" CreditResponseID="" CreditReportIdentifier="4853296|370650" /></RESPONSE_DATA></RESPONSE></RESPONSE_GROUP>
-"""
-
-xmltodict(test)
